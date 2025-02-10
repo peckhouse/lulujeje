1da3d61bbea12deb6d1179e6f24444f0e6f93a28
peckhouse <millennionprandi@gmail.com>
2025-02-10 14:59:45 +0900

    feat: Create first page.

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="coming-soon">
+    <h1>LULU & JEJE</h1>
+    <h2>Coming soon</h2>
+  </div>
+</template>
+
+<style>
+.coming-soon {
+  transform: translate3d(-50%, -50%, 0);
+  position: absolute;
+  text-align: center;
+  left: 50%;
+  top: 50%;
+}
+</style>
