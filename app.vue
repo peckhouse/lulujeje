c63ea39218b899274c9c775ebc0861f89c082cef
peckhouse <millennionprandi@gmail.com>
2025-02-10 17:42:03 +0900

    temp: Add Background, title and description

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    
   </div>
   <NuxtPage />
 </template>
+
+<style lang="scss">
+  @use "~/assets/scss/main.scss";
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  experimental: { appManifest: false },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      'Rouge+Script': true
+    }
+  }
 })
